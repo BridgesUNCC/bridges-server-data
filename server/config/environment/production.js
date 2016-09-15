a6012fd4ab7ea2306deb92711b1032b89ce6fba3
squeetus <burlinsond2@gmail.com>
2016-09-15 17:04:59 -0400

    setting up db access

--- server/config/environment/production.js
+++ server/config/environment/production.js
@@ -18,6 +18,6 @@ module.exports = {
     uri:    process.env.MONGOLAB_URI ||
             process.env.MONGOHQ_URL ||
             process.env.OPENSHIFT_MONGODB_DB_URL+process.env.OPENSHIFT_APP_NAME ||
-            'mongodb://localhost/bridgesdata'
-  }
+            'mongodb://dburlins:tittysprinkles@ds033066.mlab.com:33066/heroku_cnzcqzsr'
+    }
 };
