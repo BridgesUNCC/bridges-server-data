59176180b16ae266e7e16acbe39d064ed62ba59f
squeetus <burlinsond2@gmail.com>
2016-09-16 15:22:40 -0400

    added nested attribute to the test data model

--- server/api/test/test.model.js
+++ server/api/test/test.model.js
@@ -6,7 +6,11 @@ var mongoose = require('mongoose'),
 var TestSchema = new Schema({
   attr1: String,
   attr2: Number,
-  attr3: Boolean
+  attr3: Boolean,
+  attr4: {
+    attr5: String,
+    attr6: Number
+  }
 });
 
 module.exports = mongoose.model('Test', TestSchema);
