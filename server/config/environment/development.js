bebae7f630ffb394eb6c0ca2beb758a45ab441c3
squeetus <burlinsond2@gmail.com>
2016-09-27 14:51:52 -0400

    Added mongo credentials to environment variable for container (uses local.env.js for local host development)

--- server/config/environment/development.js
+++ server/config/environment/development.js
@@ -1,13 +1,13 @@
 'use strict';
-
+var MONGO_CREDENTIALS = require('../local.env.js').MONGO_CREDENTIALS || process.env.MONGO_CREDENTIALS;
 // Development specific configuration
 // ==================================
 module.exports = {
   // MongoDB connection options
   mongo: {
     // uri: 'mongodb://localhost/bridgesdata-dev'
-    uri: 'mongodb://admin:password@mongo:27017'   // connect to mongo container (when running on container)
-    // uri: 'mongodb://admin:password@localhost:3333' // connect to mongo container (when running on host)
+    uri: 'mongodb://' + MONGO_CREDENTIALS + '@mongo:27017'   // connect to mongo container (when running on container)
+    // uri: 'mongodb://' + MONGO_CREDENTIALS + '@localhost:3333' // connect to mongo container (when running on host)
   },
 
   seedDB: true
