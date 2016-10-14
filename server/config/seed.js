d0f32a9b7b95d970fa18ca150c509f91f841c347
squeetus <burlinsond2@gmail.com>
2016-10-14 15:37:14 -0400

    No more game seed; dataset is stable

--- server/config/seed.js
+++ server/config/seed.js
@@ -6,11 +6,11 @@
 'use strict';
 // Insert seed models below
 var Test = require('../api/test/test.model');
-var Game = require('../api/game/game.model').model;
+// var Game = require('../api/game/game.model').model;
 
 // // Insert seed data below
 var testSeed = require('../api/test/test.seed.json');
-var gameSeed = require('../api/game/game.seed.json');
+// var gameSeed = require('../api/game/game.seed.json');
 
 // Insert seed inserts below
 Test.find({}).remove(function() {
@@ -18,7 +18,6 @@ Test.find({}).remove(function() {
 });
 
 // Insert all games (17534 records)
-Game.find({}).remove(function() {
-  console.log('seeding games');
-  Game.create(gameSeed);
-});
+// Game.find({}).remove(function() {
+//   Game.create(gameSeed);
+// });
