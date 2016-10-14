565d86a877689ed6047e8212d96db875a1935b4d
squeetus <burlinsond2@gmail.com>
2016-10-14 15:22:21 -0400

    Modified Mongoose credentials to remove Docker dependence

--- server/config/environment/development.js
+++ server/config/environment/development.js
@@ -5,8 +5,8 @@ var MONGO_CREDENTIALS = process.env.MONGO_CREDENTIALS || require('../local.env.j
 module.exports = {
   // MongoDB connection options
   mongo: {
-    // uri: 'mongodb://localhost/bridgesdata-dev'
-    uri: 'mongodb://' + MONGO_CREDENTIALS + '@mongo:27017'   // connect to mongo container (when running on container)
+    uri: 'mongodb://localhost/bridgesdata-dev'
+    // uri: 'mongodb://' + MONGO_CREDENTIALS + '@mongo:27017'   // connect to mongo container (when running on container)
     // uri: 'mongodb://' + MONGO_CREDENTIALS + '@localhost:3333' // connect to mongo container (when running on host)
   },
 
