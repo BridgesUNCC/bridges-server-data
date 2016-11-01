5cb927d6996d9f881a94f785bb7c327361eecefe
squeetus <burlinsond2@gmail.com>
2016-10-31 21:14:17 -0400

    pointed test code to the dev database, added tests for Game routes

--- server/api/game/game.spec.js
+++ server/api/game/game.spec.js
@@ -20,3 +20,57 @@ describe('GET /api/games', function() {
       });
   });
 });
+
+describe('GET /api/games?limit=10', function() {
+
+  it('should respond with JSON object, and contain an object and an Array of 10 objects', function(done) {
+    request(app)
+      .get('/api/games?limit=10')
+      .expect(200)
+      .expect('Content-Type', /json/)
+      .end(function(err, res) {
+        if (err) return done(err);
+        res.body.should.be.instanceof(Object);
+        res.body.structure.should.be.instanceOf(Object);
+        res.body.data.should.be.instanceOf(Array);
+        res.body.data.length.should.be.equal(10);
+        done();
+      });
+  });
+});
+
+describe('GET /api/games?limit=something', function() {
+
+  it('should respond with JSON object, and contain an object and an Array of 17534 objects', function(done) {
+    request(app)
+      .get('/api/games?limit=something')
+      .expect(200)
+      .expect('Content-Type', /json/)
+      .end(function(err, res) {
+        if (err) return done(err);
+        res.body.should.be.instanceof(Object);
+        res.body.structure.should.be.instanceOf(Object);
+        res.body.data.should.be.instanceOf(Array);
+        res.body.data.length.should.be.equal(17534);
+        done();
+      });
+  });
+});
+
+describe('GET /api/games?limit=-1', function() {
+
+  it('should respond with JSON object, and contain an object and an Array of 17534 objects', function(done) {
+    request(app)
+      .get('/api/games?limit=-1')
+      .expect(200)
+      .expect('Content-Type', /json/)
+      .end(function(err, res) {
+        if (err) return done(err);
+        res.body.should.be.instanceof(Object);
+        res.body.structure.should.be.instanceOf(Object);
+        res.body.data.should.be.instanceOf(Array);
+        res.body.data.length.should.be.equal(17534);
+        done();
+      });
+  });
+});
--- server/config/environment/test.js
+++ server/config/environment/test.js
@@ -5,6 +5,6 @@
 module.exports = {
   // MongoDB connection options
   mongo: {
-    uri: 'mongodb://localhost/bridgesdata-test'
+    uri: 'mongodb://localhost/bridgesdata-dev'
   }
 };
