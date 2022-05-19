b6c85d2036fccf085c22115df610b07238bf78c9
quaiquai <mmcquaig@uncc.edu>
2022-05-19 12:13:55 -0400

    world cities data

---
+++ server/api/world_cities/world_cities.model.js
@@ -0,0 +1,30 @@
+'use strict';
+
+var mongoose = require('mongoose'),
+    Schema = mongoose.Schema;
+
+var structure = {
+  'id': 'String',
+  'city': 'String',
+  'state': 'String',
+  'country': 'String',
+  'lat': 'Number',
+  'lon': 'Number',
+  'elevation': 'String',
+  'population': 'Number',
+  'timezone': 'String'
+};
+
+var WorldCitySchema = new Schema({
+  id: String,
+  city: String,
+  state: String,
+  country: String,
+  lat: Number,
+  lon: Number,
+  elevation: String,
+  population: Number,
+  timezone: String
+});
+
+module.exports = {'model': mongoose.model('WorldCity', WorldCitySchema), 'structure': structure};
